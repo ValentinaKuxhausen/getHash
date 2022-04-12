4b37dafc20e0b6cd8f202b92e47dfb546097390c
Valentina-developer <valentina.kuxhausen@web.de>
2022-04-12 09:39:41 +0200

    test bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
   ],
   imports: [
     BrowserModule
